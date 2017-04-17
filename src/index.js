3ff7f312d23c5d74a0d2548db3678f502c17e290
wesley-moy <wesley.moy@codazen.com>
2017-04-17 11:11:42 -0700

    starting point

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AddressBook from './components/AddressBook.react';
-import AddContacts from './components/AddContacts.react';
-import EditContacts from './components/EditContacts.react';
 import Root from './components/Root.react';
 import {Router, Route, IndexRoute} from 'react-router';
 
 const routes = (
   <Router>
     <Route path='/' component={Root}>
-      <IndexRoute component={AddressBook} />
-      <Route path='/AddContacts' component={AddContacts} />
-      <Route path='/AddressBook' component={AddressBook} />
-      <Route path='/EditContacts/:id' component={EditContacts} />
     </Route>
   </Router>
 );
